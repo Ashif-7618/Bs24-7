7ecd4942010da9965b4a91c8140b1ef14bd031c8
Ashif A.K <124223890+Ashif-7618@users.noreply.github.com>
2026-02-26 16:09:34 +0530

    Update vite.config.ts
    Viteup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
   return {
-    base: './',         // 🟢 Add this!
+    base: '',         // 🟢 Add this!
+    build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+    }
     server: {
       port: 3000,
       host: '0.0.0.0',
